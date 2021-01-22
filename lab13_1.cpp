4f21f7a79305c44f9a5ce3f5618efa051232e10a
TunyakarnK <tunyakarn2544@gmail.com>
2021-01-22 17:31:08 +0700

    lab13

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -3,9 +3,25 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	int j;  
+	T temp;
+    for (int i=1; i<N; i++) 
+    {  
+    cout<<"Pass "<<i<<":";
+        temp = d[i];  
+        j = i-1; 
+		 
+        while (j >= 0 && d[j] < temp) 
+        {  
+            d[j+1] = d[j];  
+            j = j-1;  	
+        }  
+        d[j+1] = temp;
+        for(int k=0; k<N; k++){
+		cout << d[k] << " ";}
+		cout << "\n";
+} 
 }
-
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
 	cout << "Input Array:";
